d8113f9632c0e2e359da803b5077f8c7f35d4c73
milka_deluxe <milagalko1@gmail.com>
2021-05-05 02:41:25 +0300

    x & y coordinates

--- Laba5_op/Laba5_op/libraries.h
+++ Laba5_op/Laba5_op/libraries.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <filesystem>
 #include <windows.h>
+#include <math.h>
 using namespace std;
 namespace fs = std::filesystem;
 
@@ -15,6 +16,6 @@ struct Dot
 	float longitude;
 	string data;
 
-	float x;
-	float y;
+    float x = 6371 * cos(latitude * MP_PI / 180) * (longitude * MP_PI / 180);
+	float y = 6371 * (latitude * MP_PI / 180);
 };
