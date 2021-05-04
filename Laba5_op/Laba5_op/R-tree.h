942b20a189abf3b5a091200b7422c5ec81e13f95
Vostok17 <Gagarin.Artem.2008@gmail.com>
2021-05-04 19:52:58 +0300

    Add tree, node, dot classes

--- Laba5_op/Laba5_op/R-tree.h
+++ Laba5_op/Laba5_op/R-tree.h
@@ -1,2 +1,65 @@
 #pragma once
 #include "libraries.h"
+#define MaxEntries = 16;
+#define MinEntries = 2;
+
+
+struct Dot
+{
+	float latitude;
+	float Longitude;
+	string data;
+};
+
+class Node
+{
+public:
+	Node();
+	int count;
+	Node* child_1;
+	Node* child_2;
+	
+	void set_MBR(float, float, float, float);
+	float left;
+	float top; 
+	float right;
+	float bottom;
+
+	vector<Dot> dots;
+};
+
+Node::Node()
+{
+	count = 0;
+	child_1 = nullptr;
+	child_2 = nullptr;
+	left = 0;
+	top = 0;
+	right = 0;
+	bottom = 0;
+}
+
+inline void Node::set_MBR(float left, float top, float right, float bottom)
+{
+
+}
+
+class Tree
+{
+public:
+	Tree();
+	~Tree();
+
+	
+private:
+	Node* root;
+};
+
+Tree::Tree()
+{
+	root = nullptr;
+}
+
+Tree::~Tree()
+{
+}
